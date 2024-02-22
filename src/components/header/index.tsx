1b1239691796a886f1f45b1b5d6ca0c5f73679d2
Ogabek <mustafoev0806@gmail.com>
2024-02-22 23:45:04 +0500

    fixed: some headings

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,7 @@
 import { HeaderTop } from '@/components/header/header-top';
 import { TopCategories } from '@/components/top-categories';
 import { useLocaleLink } from '@/hooks/locale-link';
-import { logo_icon, search_icon } from '@/mock';
+import { search_icon } from '@/mock';
 import { headerData } from '@/mock/header.data';
 import { topCategories } from '@/mock/top-categories.data';
 import { TLocale } from '@/types';
@@ -28,9 +28,9 @@ export const Header = ({ lang }: IHeader) => {
   const onWindowScroll = () => {
     const scrollHeight = window.scrollY;
     if (scrollHeight > 40) {
-       setIsFixed(true);
+      setIsFixed(true);
     } else {
-       setIsFixed(false);
+      setIsFixed(false);
     }
   };
 
@@ -45,65 +45,61 @@ export const Header = ({ lang }: IHeader) => {
   return (
     <header className="container">
       <HeaderTop data={headerData.headerTop} />
-      <nav
-        className={`${isFixed && 'fixed container shadow-lg top-0 left-0 w-full z-[1001] bg-white'} flex justify-between gap-5 py-3 items-center`}
-      >
-        <div className="flex gap-7">
-          <Image
-            src={logo_icon.src}
-            alt="logo icon"
-            className="rounded-none"
-          />
-          <Button
-            color="primary"
-            onClick={() => setIsCatalogOpen((prev) => !prev)}
-            startContent={<MenuButton isActive={isCatalogOpen} />}
-            size="lg"
-          >
-            {t('catalog')}
-          </Button>
-        </div>
-        <div className="flex-1">
-          <Input
-            size="sm"
-            type="text"
-            placeholder={t('search')}
-            variant="bordered"
-            radius="lg"
-            classNames={{
-              innerWrapper: 'bg-transparent p-0 shadow-none border-none',
-              inputWrapper: 'bg-transparent p-0 shadow-none border',
-              input: 'bg-transparent border-none pl-5 shadow-none',
-            }}
-            endContent={
-              <Button
-                color="primary"
-                size="lg"
+      <nav className={isFixed ? 'fixed shadow-lg top-0 left-0 w-full z-[1001] bg-white' : ''}>
+        <div className="flex container justify-between gap-5 py-3 items-center">
+          <div className="flex gap-7 items-center">
+            <h1 className="font-bold text-2xl text-primary">MEGABOX</h1>
+            <Button
+              color="primary"
+              onClick={() => setIsCatalogOpen((prev) => !prev)}
+              startContent={<MenuButton isActive={isCatalogOpen} />}
+              size="lg"
+            >
+              {t('catalog')}
+            </Button>
+          </div>
+          <div className="flex-1">
+            <Input
+              size="sm"
+              type="text"
+              placeholder={t('search')}
+              variant="bordered"
+              radius="lg"
+              classNames={{
+                innerWrapper: 'bg-transparent p-0 shadow-none border-none',
+                inputWrapper: 'bg-transparent p-0 shadow-none border',
+                input: 'bg-transparent border-none pl-5 shadow-none',
+              }}
+              endContent={
+                <Button
+                  color="primary"
+                  size="lg"
+                >
+                  <Image
+                    src={search_icon.src}
+                    alt="search icon"
+                    className="w-6 invert"
+                  />
+                </Button>
+              }
+            />
+          </div>
+          <ul className="hidden lg:flex gap-4 items-center">
+            {headerData.headerLink.map(({ key, classes, icon, url }, idx) => (
+              <li
+                key={idx}
+                className={`flex ${classes} text-sm font-semibold cursor-pointer flex-col gap-1 items-center justify-center`}
               >
                 <Image
-                  src={search_icon.src}
-                  alt="search icon"
-                  className="w-6 invert"
+                  src={icon.src}
+                  alt={key + ' icon'}
+                  className="w-8"
                 />
-              </Button>
-            }
-          />
+                <LocaleLink href={url as string}>{t(key)}</LocaleLink>
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul className="hidden lg:flex gap-4 items-center">
-          {headerData.headerLink.map(({ key, classes, icon, url }, idx) => (
-            <li
-              key={idx}
-              className={`flex ${classes} text-sm font-semibold cursor-pointer flex-col gap-1 items-center justify-center`}
-            >
-              <Image
-                src={icon.src}
-                alt={key + ' icon'}
-                className="w-8"
-              />
-              <LocaleLink href={url as string}>{t(key)}</LocaleLink>
-            </li>
-          ))}
-        </ul>
       </nav>
       <MobileNav lang={lang} />
       <TopCategories
