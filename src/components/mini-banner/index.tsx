b2268d9c168a8ef5f0325b75bf27f84c44dfc6dd
Diyorbekkdev <diyorbekjuraev77@gmail.com>
2024-02-25 01:42:56 +0500

    Ui is updated

--- src/components/mini-banner/index.tsx
+++ src/components/mini-banner/index.tsx
@@ -1,8 +1,26 @@
-import { ISlide } from '@/mock/slider.data';
+// import arrowIcon from '@/icons/arrow-right.svg';
+import { IMiniBanner } from '@/mock/slider.data';
 
 import { FC } from 'react';
 
-export const MiniBanner: FC<ISlide> = (props) => {
-  // const { url } = props;
-  return <div className="w-full h-full"></div>;
+import { Image } from '@nextui-org/react';
+import Link from 'next/link';
+
+export const MiniBanner: FC<IMiniBanner> = (props) => {
+  const { url, imageUrl } = props;
+  return (
+    <div className="w-full relative">
+      <Image
+        src={imageUrl}
+        alt=""
+        className="object-cover w-full"
+      />
+      <Link
+        href="#"
+        className=" flex absolute items-center gap-2 top-0"
+      >
+        <span className="text-blue-600">Barchasini ko'rish {'->'}</span>
+      </Link>
+    </div>
+  );
 };
